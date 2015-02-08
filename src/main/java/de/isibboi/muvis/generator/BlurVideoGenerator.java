7c95d0a3cd25a716f43eca7c56e080a58c0487cd
Sebastian Schmidt <isibboi@gmail.com>
2015-02-08 18:24:33 +0100

    Added some blur to BlurVideoGenerator

--- src/main/java/de/isibboi/muvis/generator/BlurVideoGenerator.java
+++ src/main/java/de/isibboi/muvis/generator/BlurVideoGenerator.java
@@ -7,12 +7,12 @@ public class BlurVideoGenerator extends FloatBufferedVideoGenerator {
 	private final EffectQueue effects = new EffectQueue();
 
 	public BlurVideoGenerator() {
-		effects.addEffect(new ExponentialFadeEffect(0.7));
-//		effects.addEffect(new MatrixEffect(new float[]{
-//				0.025f, 0.05f, 0.05f,
-//				0.125f, 0.05f, 0.05f,
-//				0.5f, 0.125f, 0.025f}, 3, 3, WrapMode.CLAMP));
-		effects.addEffect(new PixelNoiseRingEffect(1, 2400, getSinTable()));
+		effects.addEffect(new ExponentialFadeEffect(0.05));
+		effects.addEffect(new MatrixEffect(new float[]{
+				0.025f, 0.05f, 0.05f,
+				0.125f, 0.05f, 0.05f,
+				0.5f, 0.125f, 0.025f}, 3, 3, WrapMode.CLAMP));
+		effects.addEffect(new PixelNoiseRingEffect(0.5, 2400, getSinTable()));
 	}
 
 	@Override
